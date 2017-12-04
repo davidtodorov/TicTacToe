52b537a1e7975ac89e079a105b5870928355e549
David Todorov <David.Todorov@fourth.com>
2017-12-04 11:39:39 +0200

    Added game validation

--- src/TicTacToe.Services.Interfaces/Models/UserRegistrationInput.cs
+++ src/TicTacToe.Services.Interfaces/Models/UserRegistrationInput.cs
@@ -7,5 +7,7 @@
         public string LastName { get; set; }
 
         public string PhotoUrl { get; set; }
+
+        public string RegistrationDate { get; set; }
     }
 }
--- src/TicTacToe.Services/GameResultValidator.cs
+++ src/TicTacToe.Services/GameResultValidator.cs
@@ -9,7 +9,104 @@ namespace TicTacToe.Services
         /// <inheritdoc />
         public GameResult GetGameResult(string board, int row, int col)
         {
-            throw new NotImplementedException();
+            var selectedPosition = row + col;
+            char playerChar = ' '; 
+            GameResult gameResult;
+
+            // Invalid Position
+            if (row > 2 || row < 0)
+            {
+                throw new NotImplementedException();
+            }
+
+            // Invalid Position
+            if (col > 2 || col < 0)
+            {
+                throw new NotImplementedException();
+            }
+
+            // Invalid Postion
+            if (board[selectedPosition] == 'X' || board[selectedPosition] == 'O')
+            {
+                throw new NotImplementedException();
+            }
+
+            //Check if it's not finished
+            if (board.Contains("-"))
+                {
+                    gameResult = GameResult.NotFinished;
+                }
+
+            //Check for player X
+            if (playerChar == 'X')
+            {
+                gameResult = GameResult.WonByX;
+            }
+
+            //Check for player O
+            else if (playerChar == 'O')
+                {
+                    gameResult = GameResult.WonByO;
+
+                }
+
+            //Check for invalid player char
+            else
+                {
+                    throw new NotImplementedException();
+                }
+
+            //Won, diagonal
+            if (board[0] == playerChar && board[4] == playerChar && board[8] == playerChar)
+                {
+                    return gameResult;
+                }
+
+            //Won, diagonal
+            if (board[2] == playerChar && board[4] == playerChar && board[6] == playerChar)
+                {
+                    return gameResult;
+                }
+
+            //Won, row 1
+            if (board[0] == playerChar && board[1] == playerChar && board[2] == playerChar)
+                {
+                    return gameResult;
+                }
+
+            //Won, row 2
+            if (board[3] == playerChar && board[4] == playerChar && board[5] == playerChar)
+                {
+                    return gameResult;
+                }
+
+            //Won, row 3
+            if (board[6] == playerChar && board[7] == playerChar && board[8] == playerChar)
+                {
+                    return gameResult;
+                }
+
+            //Won, column 1
+            if (board[0] == playerChar && board[3] == playerChar && board[6] == playerChar)
+                {
+                    return gameResult;
+                }
+
+            //Won, column 2
+            if (board[1] == playerChar && board[4] == playerChar && board[7] == playerChar)
+                {
+                    return gameResult;
+                }
+
+            //Won, column 3
+            if (board[0] == playerChar && board[3] == playerChar && board[6] == playerChar)
+                {
+                    return gameResult;
+                }
+
+            gameResult = GameResult.Draw;
+
+            return gameResult;
         }
     }
 }
