1ed9bbde92e33ebff45cc33291e1e1602675a6a5
Martin Nikolov <flextry@gmail.com>
2017-12-11 14:05:05 +0200

    Added PrintBoard() in GameEngine class

---
+++ src/TicTacToe.ConsoleApp/GameEngine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TicTacToe.ConsoleApp
+{
+    public class GameEngine
+    {
+        public void PrintBoard(string board)
+        {
+            Console.WriteLine(new string('-', 9));
+
+            for (int i = 0, cell = 0; i < 3; i++)
+            {
+                Console.Write("| ");
+
+                for (var j = 0; j < 3; j++)
+                {
+                    Console.Write($"{board[cell++]} ");
+                }
+
+                Console.WriteLine("|");
+            }
+
+            Console.WriteLine(new string('-', 9));
+        }
+    }
+}
