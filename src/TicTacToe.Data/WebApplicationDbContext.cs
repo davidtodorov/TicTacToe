8b2d19565fd0c63e6e459ce56c7aaab5b0201ec1
David Todorov <David.Todorov@fourth.com>
2018-01-09 12:01:08 +0200

    1. Added inherit class in User 2. Added WebAppDbContext in TicTacToe.Data

---
+++ src/TicTacToe.Data/WebApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TicTacToe.Models;
+
+namespace TicTacToe.Data
+{
+    public class WebApplicationDbContext : IdentityDbContext<User>
+    {
+        public WebApplicationDbContext(DbContextOptions<TicTacToeDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
--- src/TicTacToe.Models/User.cs
+++ src/TicTacToe.Models/User.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 
 namespace TicTacToe.Models
 {
-    public class User
+    public class User : IdentityUser
     {
         public User()
         {
